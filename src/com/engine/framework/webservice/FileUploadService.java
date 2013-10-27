05948f14c4e41e91639ee7c20811e40540f8eeea
Phillip Cui <ronaldphill33@gmail.com>
2013-10-27 15:47:06 +0800

    additional changes to fileuploadservice

--- src/com/engine/framework/webservice/FileUploadService.java
+++ src/com/engine/framework/webservice/FileUploadService.java
@@ -8,7 +8,6 @@ package com.engine.framework.webservice;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -16,7 +15,6 @@ import java.net.URL;
 import org.apache.http.HttpStatus;
 
 import com.engine.framework.enumerations.ResponseStatus;
-import com.engine.framework.helper.WebServiceHelper;
 import com.engine.framework.webservice.interfaces.WebServiceListener;
 import com.engine.framework.webservice.response.Response;
 
@@ -65,6 +63,7 @@ public class FileUploadService extends AsyncTask<WebServiceInfo,Integer,Response
 		WebServiceInfo wsInfo = params[0];
 		File file = wsInfo.getUploadFile();
 		String fileName = file.getName();
+		
 		Response response = new Response();
 		
 		if(file != null && file.isFile()) {
@@ -82,55 +81,66 @@ public class FileUploadService extends AsyncTask<WebServiceInfo,Integer,Response
 				
 				 
                    FileInputStream fileInputStream = new FileInputStream( file );
-                   conn = WebServiceHelper.uploadFile(wsInfo);
+                   URL url = new URL( wsInfo.getUrl() );
+                    
+                   // Open a HTTP  connection to  the URL
+                   conn = (HttpURLConnection) url.openConnection(); 
+                   conn.setDoInput(true); // Allow Inputs
+                   conn.setDoOutput(true); // Allow Outputs
+                   conn.setUseCaches(false); // Don't use a Cached Copy
+                   conn.setRequestMethod("POST");
+                   conn.setRequestProperty("Connection", "Keep-Alive");
+                   conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                   conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                   conn.setRequestProperty("uploaded_file", fileName ); 
+                    
+                   dos = new DataOutputStream(conn.getOutputStream());
+          
+                   dos.writeBytes(twoHyphens + boundary + lineEnd); 
+                   dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                                             + fileName + "\"" + lineEnd);
+                    
+                   dos.writeBytes(lineEnd);
+          
+                   // create a buffer of  maximum size
+                   bytesAvailable = fileInputStream.available(); 
+          
+                   bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                   buffer = new byte[bufferSize];
+          
+                   // read file and write it into form...
+                   bytesRead = fileInputStream.read(buffer, 0, bufferSize);  
+                      
+                   while (bytesRead > 0) {
+                        
+                     dos.write(buffer, 0, bufferSize);
+                     bytesAvailable = fileInputStream.available();
+                     bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                     bytesRead = fileInputStream.read(buffer, 0, bufferSize); 
+                     
+                    }
+          
+                   // send multipart form data necesssary after file data...
+                   dos.writeBytes(lineEnd);
+                   dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+          
+                   // Responses from the server (code and message)
+                   int serverResponseCode = conn.getResponseCode();
+                   String serverResponseMessage = conn.getResponseMessage();
                    
-                   if(conn != null) {
-                	   
-	                   dos = new DataOutputStream(conn.getOutputStream());
-	          
-	                   dos.writeBytes(twoHyphens + boundary + lineEnd); 
-	                   dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
-	                                             + fileName + "\"" + lineEnd);
-	                    
-	                   dos.writeBytes(lineEnd);
-	          
-	                   // create a buffer of  maximum size
-	                   bytesAvailable = fileInputStream.available(); 
-	          
-	                   bufferSize = Math.min(bytesAvailable, maxBufferSize);
-	                   buffer = new byte[bufferSize];
-	          
-	                   // read file and write it into form...
-	                   bytesRead = fileInputStream.read(buffer, 0, bufferSize);  
-	                      
-	                   while (bytesRead > 0) {
-	                        
-	                     dos.write(buffer, 0, bufferSize);
-	                     bytesAvailable = fileInputStream.available();
-	                     bufferSize = Math.min(bytesAvailable, maxBufferSize);
-	                     bytesRead = fileInputStream.read(buffer, 0, bufferSize);   
-	                      
-	                    }
-	          
-	                   // send multipart form data necesssary after file data...
-	                   dos.writeBytes(lineEnd);
-	                   dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-	          
-	                   // Responses from the server (code and message)
-	                   int serverResponseCode = conn.getResponseCode();
-	                   String serverResponseMessage = conn.getResponseMessage();
-	                   
-	                   Log.i("uploadFile", "HTTP Response is : " + serverResponseMessage + ": " + serverResponseCode);  
-	                   
-	                   if( serverResponseCode == HttpStatus.SC_OK ){
-	                	    response.setStatus(ResponseStatus.SUCCESS);
-	                   }    
-	                    
-	                   //close the streams //
-	                   fileInputStream.close();
-	                   dos.flush();
-	                   dos.close();
-                   }	
+                   Log.i("uploadFile", "HTTP Response is : " + serverResponseMessage + ": " + serverResponseCode);  
+                   
+                   if( serverResponseCode == HttpStatus.SC_OK ){
+                	    response.setStatus(ResponseStatus.SUCCESS);
+                   }    
+                    
+                   //close the streams //
+                   fileInputStream.close();
+                   dos.flush();
+                   dos.close();
+                   
+           	    	return response;
+				
 			}
 			catch(MalformedURLException e) {
 				e.printStackTrace();
